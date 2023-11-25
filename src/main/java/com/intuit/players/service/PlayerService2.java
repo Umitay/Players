22a25e32794a3c8036d4d059a93366b22814be5e
Umitay <umitay.turdykulov@gmail.com>
2023-11-25 11:18:42 +0200

    Added Controller Test, Next is Component Test

---
+++ src/main/java/com/intuit/players/service/PlayerService2.java
@@ -0,0 +1,28 @@
+package com.intuit.players.service;
+
+import com.intuit.players.model.Player;
+import com.intuit.players.repository.IReactiveMongoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@Service
+public class PlayerService {
+    private final IReactiveMongoRepository playerRepository;
+
+    @Autowired
+    public PlayerService(IReactiveMongoRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public Mono<Player> getPlayerById(String playerId) {
+         return playerRepository.findById(playerId);
+    }
+
+    public Flux<Player> getAllPlayers() {
+        return playerRepository.findAll();
+    }
+}
---
+++ src/test/java/com/intuit/players/component/PlayerComponentTest.java
@@ -0,0 +1,47 @@
+package com.intuit.players.controller;
+
+import com.intuit.players.config.TestConfig;
+import com.intuit.players.model.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+@SpringBootTest
+public class PlayerComponentTest {
+
+@Autowired
+public PlayerController playerController;
+    public WebTestClient webClient;
+
+    @BeforeEach
+    void before(){
+        webClient = WebTestClient.bindToController(playerController).build();
+    }
+
+    @Test
+    void getAllPlayers() {
+        webClient.get().uri("/api/players")
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Player.class)
+                .getResponseBody()
+                .as(StepVerifier::create)// Convert the Flux to StepVerifier
+                .verifyComplete();
+    }
+
+    @Test
+    void getPlayerById() {
+
+        String playerId = "abbated01";
+
+        //TODO Use StepVerifier to verify the reactive behavior
+        webClient.get().uri("/api/players/{playerId}", playerId)
+                .exchange()
+                .expectStatus().isOk();
+    }
+}
